1428592f225bdad556b9564e350dbda199f52360
AkashNayak6 <46473508+AkashNayak6@users.noreply.github.com>
2019-06-16 12:57:56 -0400

    Update guessing-game.py

--- guessing-game.py
+++ guessing-game.py
@@ -7,12 +7,12 @@ guesses = 0
 while guesses < 100:
     guess = int(input("Enter an integer from 1 to 99: "))
     guesses += 1
-    print("this is your %d guess")
+    print("this is your guess")
 
     if guess < number:
-        print("guess is low")
+        print("your guess is low")
     elif guess > number:
-        print("guess is high")
+        print("your guess is high")
     elif guess == number:
-        print("guess correct!")
+        print("your guess is correct, congratulations!")
         break
